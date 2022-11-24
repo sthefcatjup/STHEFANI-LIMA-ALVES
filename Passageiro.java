03e51ce34ac9793915638c541bc7bf1f34f49066
sthefcatjup <111788708+sthefcatjup@users.noreply.github.com>
2022-11-24 18:11:34 -0300

    Create Passageiro.java

---
+++ Passageiro.java
@@ -0,0 +1,23 @@
+Passageiro.java
+
+
+public class Teste {
+    public static void main(String[] args){
+        //Sthefani Lima 
+        //usando o encapsulamento
+        Passageiro m = new Passageiro();
+        m.setNome("Sthef");
+        m.setIdade(18);
+        
+        CalculadoradeTarifa c = new CalculadoradeTarifa();
+        System.out.println("O valor da corrida foi: ");
+        c.valorCorrida(20);
+
+        Viagem v = new Viagem();
+        v.setLocalizacão("São Paulo");
+
+        v.realizarviagem(s);
+        v.viagemFinalizada(s);
+
+    }
+}
